08b5350e7151003dd98d3456421e66a4d00bd9c6
yintianchao <yintianchao@icinfo.com.cn>
2019-11-05 16:55:29 +0800

    feat:logging日志

--- src/main/java/com/ytc/springboot/HelloController.java
+++ src/main/java/com/ytc/springboot/HelloController.java
@@ -1,5 +1,7 @@
 package com.ytc.springboot;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,8 +13,11 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @RestController
 public class HelloController {
+    private static final Logger logger = LoggerFactory.getLogger(HelloController.class);
+
     @RequestMapping("/hello")
     public String hello(){
+        logger.info("=================hello==============");
         return "hello springboot";
     }
 }
