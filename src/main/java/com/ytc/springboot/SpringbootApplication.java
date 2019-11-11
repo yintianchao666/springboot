ebee730a5460d85b4c0bdb4228d74a522e477be9
yintianchao <yintianchao@icinfo.com.cn>
2019-11-11 09:20:11 +0800

    feat:更改公共配置文件名称

--- src/main/java/com/ytc/springboot/SpringbootApplication.java
+++ src/main/java/com/ytc/springboot/SpringbootApplication.java
@@ -2,12 +2,16 @@ package com.ytc.springboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 
 @SpringBootApplication
 public class SpringbootApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(SpringbootApplication.class, args);
+       // SpringApplication.run(SpringbootApplication.class, args);
+        //变更配置文件读取位置启动
+        new SpringApplicationBuilder(SpringbootApplication.class)
+                .properties("spring.config.location=classpath:/springbootconfig.properties").run(args);
     }
 
 }
